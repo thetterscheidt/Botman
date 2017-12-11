1a550c5c417a7f8098a5addb030f8293088478bb
Tom Hetterscheidt <thetterscheidt@gmail.com>
2017-12-10 19:36:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,94 +1,51 @@
-import os
-import sys
-import json
-from datetime import datetime
+# FB messenger credentials
+ACCESS_TOKEN = os.environ["PAGE_ACCESS_TOKEN"]
 
-import requests
-from flask import Flask, request
+# api.ai credentials
+CLIENT_ACCESS_TOKEN = os.environ["VERIFY_TOKEN"]
+ai = apiai.ApiAI(CLIENT_ACCESS_TOKEN)
 
 app = Flask(__name__)
 
-
 @app.route('/', methods=['GET'])
 def verify():
-    # when the endpoint is registered as a webhook, it must echo back
-    # the 'hub.challenge' value it receives in the query arguments
+    # our endpoint echos back the 'hub.challenge' value specified when we setup the webhook
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+        if not request.args.get("hub.verify_token") == 'foo':
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
 
-    return "Hello my brutah", 200
+    return 'Totally Verified Dude!', 200
+
+def reply(user_id, msg):
+    data = {
+        "recipient": {"id": user_id},
+        "message": {"text": msg}
+    }
+    resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + ACCESS_TOKEN, json=data)
+    print(resp.content)
 
 
 @app.route('/', methods=['POST'])
-def webhook():
+def handle_incoming_messages():
+    data = request.json
+    sender = data['entry'][0]['messaging'][0]['sender']['id']
+    message = data['entry'][0]['messaging'][0]['message']['text']
 
-    # endpoint for processing incoming messaging events
+    # prepare API.ai request
+    req = ai.text_request()
+    req.lang = 'en'  # optional, default value equal 'en'
+    req.query = message
 
-    data = request.get_json()
-    log(data)  # you may not want to log every incoming message in production, but it's good for testing
+    # get response from API.ai
+    api_response = req.getresponse()
+    responsestr = api_response.read().decode('utf-8')
+    response_obj = json.loads(responsestr)
+    if 'result' in response_obj:
+        response = response_obj["result"]["fulfillment"]["speech"]
+        reply(sender, response)
 
-    if data["object"] == "page":
-
-        for entry in data["entry"]:
-            for messaging_event in entry["messaging"]:
-
-                if messaging_event.get("message"):  # someone sent us a message
-
-                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
-                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
-
-                    send_message(sender_id, "Roger that!")
-
-                if messaging_event.get("delivery"):  # delivery confirmation
-                    pass
-
-                if messaging_event.get("optin"):  # optin confirmation
-                    pass
-
-                if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
-                    pass
-
-    return "ok", 200
-
-
-def send_message(recipient_id, message_text):
-
-    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
-
-    params = {
-        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
-    }
-    headers = {
-        "Content-Type": "application/json"
-    }
-    data = json.dumps({
-        "recipient": {
-            "id": recipient_id
-        },
-        "message": {
-            "text": message_text
-        }
-    })
-    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
-    if r.status_code != 200:
-        log(r.status_code)
-        log(r.text)
-
-
-def log(msg, *args, **kwargs):  # simple wrapper for logging to stdout on heroku
-    try:
-        if type(msg) is dict:
-            msg = json.dumps(msg)
-        else:
-            msg = unicode(msg).format(*args, **kwargs)
-        print u"{}: {}".format(datetime.now(), msg)
-    except UnicodeEncodeError:
-        pass  # squash logging errors in case of non-ascii text
-    sys.stdout.flush()
-
+    return "ok"
 
 if __name__ == '__main__':
     app.run(debug=True)
